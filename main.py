65b791ee1234bc22112651241af82339bb82e9de
Rubadel <66481905+Rubadel@users.noreply.github.com>
2021-03-22 09:39:46 +0300

    .

--- main.py
+++ main.py
@@ -300,7 +300,7 @@ for x1 in lis1:
 # 5
 listr = [3, 5, 7, 9]
 print(random.choice(listr))
-"""
+
 # Exercise
 
 Ruba = set(("Hi","I","am","here","Hi"))
@@ -317,11 +317,244 @@ print(Ruba)
 print("intersection is as: ", Ruba.intersection(Raya,Ruba))
 print("Union is as: ", Ruba.union(Raya))
 
+# Lab 6
+# 1
+Ruba = set(("Hi", "I", "am", "here"))
+print("The set is:", Ruba)
+item = input("Enter item to add\n")
+Ruba.add(item)
+print("The set after added:", Ruba)
+Ruba.clear()
+cr = Ruba.copy()
+print("The copy of the set is:",cr)
+
+# 2
+def rms(Seet,f):
+
+    if f in Seet:
+        Seet.discard(f)
+
+        print(Seet)
+
+    else:
+        print("Number is not in the set")
+
+
+Seet = set((1,2,3,4))
+f=1
+rms(Seet, f)
+
+# 3
+set1 = {0,2,4}
+set2 = {1,3,5}
+
+print("The intersectio of sets is, set1.intersection(set2))
+
+
+#4
+def ch(sett,c):
+    if c in sett:
+        print("Yes is in")
+    else:
+        print("NO is not")
+
+
+sett={0,1,2}
+c=9
+ch(sett,c)
+
+
+# 5
+def ins(set1, set2):
+
+    print(set1 - set2) #or using print(set1.difference(set2))
+
+
+set1 = {1, 2, 3, 4, 5, 6}
+
+set2 = {1, 3, 5}
+
+ins(set1, set2)
+
+set1 = {0, 2, 4, 6}
+set2 = {1, 3, 5, 7}
+
+set3 = 
+
+print("The difference set is",set3)
+
+# Exercise
+student = dict(ID=38122, Name="Ruba", Level=22)
+
+print ( student.update(Age=22) )
+print( student.setdefault("5") )
+print( student.get("Level") )
+print( student.keys() )
+print (student.fromkeys("Name") )
+print( student.copy() )
+print( student.popitem())
+print(student.values())
+
+"""
+
+# Lab 7
+
+# 1
+# lab Solving
+""""
+for x in range(6):
+    if x == 3:
+        continue
+    print(x)
+# Array
+AryE = [0, 2, 4, 6, 8]
+AryO = [1, 3, 5, 7, 9]
+print("Event Array is: ", AryE, "\n", "Odd array is:", AryO,"\n")
+
+AryE.append(10)
+AryE[0]=1
+AryE.pop(4)
+for x in AryE:
+    print("The array elements are: ",x)
+
+AryO.remove(1)
+AR = AryO.extend(AryE)
+
+for x in AryO:
+    print("The array elements are: ",x,)
+
+print("AryO After cleared: " ,AryO.clear())
+
+Ruba = dict(fname="RUBA", Age=22, GPA="4.99")
+Ruba.update(Lanem= "ADEL")
+print(Ruba)
+print(Ruba.setdefault("Lname"))
+
+list = [1,5,9]
+list.insert(2,7)
+print("list",list)
+
+name='abc'
+rev='def'
+
+print(name[:2]+rev[2:])
+print(name[2:]+rev[:2])
+for i in name:
+    rev=i+rev
+
+print(rev)
+
+
+h='roror'
+fc=h[0]
+for i in h:
+    if fc == i:
+       res= h.replace(fc,'$')
+
+print(res)
+
+di1={5:'E',
+     2:'B',
+     6:'F'}
+
+di2={4:'D',
+     3:'C',
+     1:'A'}
+di3={}
+for i in (di1,di2):
+    di3.update(i)
+
+print(sorted(di3.items()))
+
+cou=0
+str=input("str")
+nu=input("NUM")
+
+for x in str:
+    if x == nu:
+        cou=1+cou
+
+print(cou);
+
+#lab 10 
+# 1
+class myClass:
+    def pyth(self):
+        print("Hello python")
+
+
+obj = myClass()
+obj.pyth()
+
+# 2
+
+class names:
+
+    def _init_(self):
+        self.fname = ""
+        self.lname =" "
+
+    def getStr(self,fname,lname):
+        self.fname = input("Enter fist name\n")
+        self.lname = input("Enter last name\n")
+
+    def printStr(self):
+        print(self.fname.upper()+" " + self.lname.upper())
+
+
+obj = names()
+obj.getStr("","")
+obj.printStr()
+ 
+# 3
+class Rectangle:
+    def __init__(self,length,width):
+        self.length = length
+        self.width = width
+
+    def computRec(self):
+        res = self.length * self.width
+        print("The area of Rectangle is = ", res)
+
+
+o1 = Rectangle(2,6)
+o1.computRec()
+
+"""
+# 4
+class person:
+    def __init__(s,fname,lname):
+        s.fname = fname
+        s.lname = lname
+
+    def getinfo(s):
+        s.fname = input("Enter first name\n")
+        s.lname = input("Enter last name\n")
+
+class Adress:
+    def __init__(j,country,city):
+        j.country = country
+        j.city = city
+
+    def getinfo(j):
+        j.country = input("Enter your country \n")
+        j.city = input("Enter your city \n")
+
+
+class Contact(person,Adress):
+    def __init__(x,fn,ln,cou,cit):
+        x.fname = fn
+        x.lname = ln
+        x.country = cou
+        x.city = cit
+
+    def contactInfo(s):
+        print("User first name: ",s.fname,"\nUser last name: ", s.lname)
+        print("User country is: ",s.country,"\nUser City is: ",s.city )
 
 
 
-
-
+obj = Contact("","","","","")
 
 
 
